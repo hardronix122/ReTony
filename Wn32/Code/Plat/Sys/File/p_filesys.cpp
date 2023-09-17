aca978839027169934e62d6d19f8a1a9f7459e08
hardronix122 <35351870+hardronix122@users.noreply.github.com>
2023-09-17 05:02:36 +0300

    p_filesys.cpp: add module path resolution for linux

--- Wn32/Code/Plat/Sys/File/p_filesys.cpp
+++ Wn32/Code/Plat/Sys/File/p_filesys.cpp
@@ -11,333 +11,305 @@
 
 // #include <Gfx/xbox/nx/nx_init.h>
 
+#ifdef __PLAT_WN32__
 #include <Windows.h>
+#endif
 
 #include <fstream>
 
-namespace File
-{
-	// File handle
-	struct sFileHandle
-	{
-		std::ifstream file;
-		size_t filesize = 0;
+namespace File {
+    // File handle
+    struct sFileHandle {
+        std::ifstream file;
+        size_t filesize = 0;
 
-		sFileHandle(const std::filesystem::path &path) : file(path, std::ios::binary)
-		{
-			if (file.is_open())
-				filesize = std::filesystem::file_size(path);
-		}
-	};
+        sFileHandle(const std::filesystem::path &path) : file(path, std::ios::binary) {
+            if (file.is_open())
+                filesize = std::filesystem::file_size(path);
+        }
+    };
 
-	// Return module path
-	static std::filesystem::path GetModulePath()
-	{
-		// Get module namethe
-		std::basic_string<WCHAR> module_name(MAX_PATH, '\0');
+    // Return module path
+    static std::filesystem::path GetModulePath() {
+        // There's no any platform-agnostic way of doing so...
+        // Well, cross-platform stuff is usually about hiding platform-specific stuff...
+        // Whatever. *depressed noises*
+#ifdef __PLAT_WN32__
+        // Get module namethe
+        std::basic_string<WCHAR> module_name(MAX_PATH, '\0');
 
-		while (1)
-		{
-			DWORD decceded = GetModuleFileNameW(nullptr, module_name.data(), module_name.size());
-			if (decceded < module_name.size())
-			{
-				module_name.resize(decceded);
-				break;
-			}
-			else
-			{
-				module_name.resize(module_name.size() * 2);
-			}
-		}
+        while (1)
+        {
+            DWORD decceded = GetModuleFileNameW(nullptr, module_name.data(), module_name.size());
+            if (decceded < module_name.size())
+            {
+                module_name.resize(decceded);
+                break;
+            }
+            else
+            {
+                module_name.resize(module_name.size() * 2);
+            }
 
-		// Remove filename
-		std::filesystem::path module_path(module_name);
-		module_path.remove_filename();
-		return module_path;
-	}
+    }
 
-	std::filesystem::path ModulePath()
-	{
-		static std::filesystem::path module_path = GetModulePath();
-		return module_path;
-	}
+        // Remove filename
+        std::filesystem::path module_path(module_name);
+        module_path.remove_filename();
+        return module_path;
+#endif
 
-	// Return data path
-	static std::filesystem::path GetDataPath()
-	{
-		// Get module path
-		return ModulePath() / "Data";
-	}
+#ifdef __PLAT_LINUX__
+        return std::filesystem::canonical("/proc/self/exe").remove_filename();
+#endif
+    }
 
-	std::filesystem::path DataPath()
-	{
-		static std::filesystem::path data_path = GetDataPath();
-		return data_path;
-	}
+    std::filesystem::path ModulePath() {
+        static std::filesystem::path module_path = GetModulePath();
+        return module_path;
+    }
 
-	static void* prefopen( const char *filename, const char *mode )
-	{
-		// Process file name
-		std::filesystem::path file_path(filename);
+    // Return data path
+    static std::filesystem::path GetDataPath() {
+        // Get module path
+        return ModulePath() / "Data";
+    }
 
-		// If name ends with .xbx, remove
-		if (file_path.extension() == ".xbx" || file_path.extension() == ".Xbx")
-			file_path.replace_extension();
+    std::filesystem::path DataPath() {
+        static std::filesystem::path data_path = GetDataPath();
+        return data_path;
+    }
 
-		// If name ends with .ps2, remove
-		if (file_path.extension() == ".ps2")
-			file_path.replace_extension();
+    static void *prefopen(const char *filename, const char *mode) {
+        // Process file name
+        std::filesystem::path file_path(filename);
 
-		// Open the file
-		std::filesystem::path full_path = DataPath() / file_path;
-		sFileHandle *h_file = new sFileHandle(full_path);
+        // If name ends with .xbx, remove
+        if (file_path.extension() == ".xbx" || file_path.extension() == ".Xbx")
+            file_path.replace_extension();
 
-		if (!h_file->file.is_open())
-		{
-			delete h_file;
-			return nullptr;
-		}
+        // If name ends with .ps2, remove
+        if (file_path.extension() == ".ps2")
+            file_path.replace_extension();
 
-		return h_file;
-	}
+        // Open the file
+        std::filesystem::path full_path = DataPath() / file_path;
+        sFileHandle *h_file = new sFileHandle(full_path);
 
-	void InstallFileSystem( void )
-	{
-	#	if 0
-		// This is where we start the thread that will deal with copying commonly used data from the DVD to the utility
-		// region (z:\) on the HD.
-		pLoader = new CThreadedLevelLoader();
+        if (!h_file->file.is_open()) {
+            delete h_file;
+            return nullptr;
+        }
 
-		SLevelDesc level_descs[] = {{ "pre\\alc.prx" },
-									{ "pre\\alccol.prx" },
-									{ "pre\\alcscn.prx" },
-									{ "pre\\anims.prx" },
-									{ "pre\\bits.prx" },
-									{ "pre\\cnv.prx" },
-									{ "pre\\cnvcol.prx" },
-									{ "pre\\cnvscn.prx" },
-									{ "pre\\fonts.prx" },
-									{ "pre\\jnk.prx" },
-									{ "pre\\jnkcol.prx" },
-									{ "pre\\jnkscn.prx" },
-									{ "pre\\kon.prx" },
-									{ "pre\\koncol.prx" },
-									{ "pre\\konscn.prx" },
-									{ "pre\\lon.prx" },
-									{ "pre\\loncol.prx" },
-									{ "pre\\lonscn.prx" },
-									{ "pre\\qb.prx" },
-									{ "pre\\sch.prx" },
-									{ "pre\\schcol.prx" },
-									{ "pre\\schscn.prx" },
-									{ "pre\\sf2.prx" },
-									{ "pre\\sf2col.prx" },
-									{ "pre\\sf2scn.prx" },
-									{ "pre\\skaterparts.prx" },
-									{ "pre\\skater_sounds.prx" },
-									{ "pre\\skateshop.prx" },
-									{ "pre\\skateshopcol.prx" },
-									{ "pre\\skateshopscn.prx" },
-									{ "pre\\skeletons.prx" },
-									{ "pre\\zoo.prx" },
-									{ "pre\\zoocol.prx" },
-									{ "pre\\zooscn.prx" }};
+        return h_file;
+    }
 
-		static BYTE data_buffer[32 * 1024];
-		pLoader->Initialize( level_descs, 34, data_buffer, 32 * 1024, &OkayToUseUtilityDrive );
-		pLoader->AsyncStreamLevel( 0 );
-	#	endif
+    void InstallFileSystem(void) {
+#	if 0
+        // This is where we start the thread that will deal with copying commonly used data from the DVD to the utility
+        // region (z:\) on the HD.
+        pLoader = new CThreadedLevelLoader();
 
-		// Initialise the async file system.
-		File::CAsyncFileLoader::sInit();
-	}
+        SLevelDesc level_descs[] = {{ "pre\\alc.prx" },
+                                    { "pre\\alccol.prx" },
+                                    { "pre\\alcscn.prx" },
+                                    { "pre\\anims.prx" },
+                                    { "pre\\bits.prx" },
+                                    { "pre\\cnv.prx" },
+                                    { "pre\\cnvcol.prx" },
+                                    { "pre\\cnvscn.prx" },
+                                    { "pre\\fonts.prx" },
+                                    { "pre\\jnk.prx" },
+                                    { "pre\\jnkcol.prx" },
+                                    { "pre\\jnkscn.prx" },
+                                    { "pre\\kon.prx" },
+                                    { "pre\\koncol.prx" },
+                                    { "pre\\konscn.prx" },
+                                    { "pre\\lon.prx" },
+                                    { "pre\\loncol.prx" },
+                                    { "pre\\lonscn.prx" },
+                                    { "pre\\qb.prx" },
+                                    { "pre\\sch.prx" },
+                                    { "pre\\schcol.prx" },
+                                    { "pre\\schscn.prx" },
+                                    { "pre\\sf2.prx" },
+                                    { "pre\\sf2col.prx" },
+                                    { "pre\\sf2scn.prx" },
+                                    { "pre\\skaterparts.prx" },
+                                    { "pre\\skater_sounds.prx" },
+                                    { "pre\\skateshop.prx" },
+                                    { "pre\\skateshopcol.prx" },
+                                    { "pre\\skateshopscn.prx" },
+                                    { "pre\\skeletons.prx" },
+                                    { "pre\\zoo.prx" },
+                                    { "pre\\zoocol.prx" },
+                                    { "pre\\zooscn.prx" }};
 
-	long GetFileSize( void* pFP )
-	{
-		Dbg_MsgAssert( pFP, ( "nullptr pFP sent to GetFileSize" ));
+        static BYTE data_buffer[32 * 1024];
+        pLoader->Initialize( level_descs, 34, data_buffer, 32 * 1024, &OkayToUseUtilityDrive );
+        pLoader->AsyncStreamLevel( 0 );
+#	endif
 
-		if( PreMgr::sPreEnabled())
-		{
-			int retval = PreMgr::pre_get_file_size( (PreFile::FileHandle *) pFP );
-			if( PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
-	
-		sFileHandle *h_file = (sFileHandle*)pFP;
-		return h_file->filesize;
-	}
+        // Initialise the async file system.
+        File::CAsyncFileLoader::sInit();
+    }
 
-	long GetFilePosition( void *pFP )
-	{
-		Dbg_MsgAssert( pFP, ( "nullptr pFP sent to GetFilePosition" ));
+    long GetFileSize(void *pFP) {
+        Dbg_MsgAssert(pFP, ("nullptr pFP sent to GetFileSize"));
 
-		if( PreMgr::sPreEnabled())
-		{
-			int retval = PreMgr::pre_get_file_position((PreFile::FileHandle*)pFP );
-			if( PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
+        if (PreMgr::sPreEnabled()) {
+            int retval = PreMgr::pre_get_file_size((PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
 
-		sFileHandle *h_file = (sFileHandle *)pFP;
-		return h_file->file.tellg();
-	}
+        sFileHandle *h_file = (sFileHandle *) pFP;
+        return h_file->filesize;
+    }
 
-	void InitQuickFileSystem( void )
-	{
-	}
+    long GetFilePosition(void *pFP) {
+        Dbg_MsgAssert(pFP, ("nullptr pFP sent to GetFilePosition"));
 
-	uint32	CanFileBeLoadedQuickly( const char* filename )
-	{
-		(void)filename;
-		return 0;
-	}
+        if (PreMgr::sPreEnabled()) {
+            int retval = PreMgr::pre_get_file_position((PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
 
-	bool LoadFileQuicklyPlease( const char* filename, uint8 *addr )
-	{
-		(void)filename;
-		(void)addr;
-		return false;
-	}
+        sFileHandle *h_file = (sFileHandle *) pFP;
+        return h_file->file.tellg();
+    }
 
-	void StopStreaming( void )
-	{
-	}
+    void InitQuickFileSystem(void) {
+    }
 
-	void UninstallFileSystem( void )
-	{
-	}
+    uint32 CanFileBeLoadedQuickly(const char *filename) {
+        (void) filename;
+        return 0;
+    }
 
-	bool Exist( const char *filename )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			bool retval = PreMgr::pre_fexist(filename);
-			if (PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
+    bool LoadFileQuicklyPlease(const char *filename, uint8 *addr) {
+        (void) filename;
+        (void) addr;
+        return false;
+    }
 
-		void *p_result = prefopen( filename, "rb" );
-		if( p_result != nullptr )
-		{
-			Close( p_result );
-		}
+    void StopStreaming(void) {
+    }
 
-		return( p_result != nullptr );
-	}
+    void UninstallFileSystem(void) {
+    }
 
-	void *Open( const char *filename, const char *access )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			void * retval = PreMgr::pre_fopen(filename, access);
-			if (PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
+    bool Exist(const char *filename) {
+        if (PreMgr::sPreEnabled()) {
+            bool retval = PreMgr::pre_fexist(filename);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
 
-		return prefopen( filename, access );
-	}
+        void *p_result = prefopen(filename, "rb");
+        if (p_result != nullptr) {
+            Close(p_result);
+        }
 
-	int Close( void *pFP )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			int retval = PreMgr::pre_fclose((PreFile::FileHandle *) pFP);
-			if (PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
+        return (p_result != nullptr);
+    }
 
-		sFileHandle *h_file = (sFileHandle *)pFP;
-		delete h_file;
-		return 0;
-	}
+    void *Open(const char *filename, const char *access) {
+        if (PreMgr::sPreEnabled()) {
+            void *retval = PreMgr::pre_fopen(filename, access);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
 
-	size_t Read( void *addr, size_t size, size_t count, void *pFP )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			size_t retval = PreMgr::pre_fread( addr, size, count, (PreFile::FileHandle*)pFP );
-			if( PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
+        return prefopen(filename, access);
+    }
 
-		sFileHandle *h_file = (sFileHandle*)pFP;
-		h_file->file.read((char*)addr, size * count);
-		return h_file->file.gcount();
-	}
-	
-	size_t ReadInt( void *addr, void *pFP )
-	{
-		return Read( addr, 4, 1, pFP );
-	}
+    int Close(void *pFP) {
+        if (PreMgr::sPreEnabled()) {
+            int retval = PreMgr::pre_fclose((PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
 
-	size_t Write( const void *addr, size_t size, size_t count, void *pFP )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			size_t retval = PreMgr::pre_fwrite(addr, size, count, (PreFile::FileHandle *) pFP);
-			if (PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
-		return 0;
-	}
+        sFileHandle *h_file = (sFileHandle *) pFP;
+        delete h_file;
+        return 0;
+    }
 
-	char * GetS( char *buffer, int maxlen, void *pFP )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			char * retval = PreMgr::pre_fgets(buffer, maxlen, (PreFile::FileHandle *) pFP);
-			if (PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
-		return nullptr;
-	}
+    size_t Read(void *addr, size_t size, size_t count, void *pFP) {
+        if (PreMgr::sPreEnabled()) {
+            size_t retval = PreMgr::pre_fread(addr, size, count, (PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
 
-	int PutS( const char *buffer, void *pFP )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			int retval = PreMgr::pre_fputs(buffer, (PreFile::FileHandle *) pFP);
-			if (PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
-		return 0;
-	}
+        sFileHandle *h_file = (sFileHandle *) pFP;
+        h_file->file.read((char *) addr, size * count);
+        return h_file->file.gcount();
+    }
 
-	int Eof( void *pFP )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			int retval = PreMgr::pre_feof((PreFile::FileHandle *) pFP);
-			if (PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
-		return 0;
-	}
+    size_t ReadInt(void *addr, void *pFP) {
+        return Read(addr, 4, 1, pFP);
+    }
 
-	int Seek( void *pFP, long offset, int origin )
-	{
-		if( PreMgr::sPreEnabled())
-		{
-			int retval = PreMgr::pre_fseek((PreFile::FileHandle *) pFP, offset, origin);
-			if( PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
+    size_t Write(const void *addr, size_t size, size_t count, void *pFP) {
+        if (PreMgr::sPreEnabled()) {
+            size_t retval = PreMgr::pre_fwrite(addr, size, count, (PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
+        return 0;
+    }
 
-		sFileHandle *h_file = (sFileHandle*)pFP;
-		h_file->file.seekg(offset, (origin == SEEK_CUR) ? std::ios::cur : ((origin == SEEK_SET) ? std::ios::beg : std::ios::end));
-		return h_file->file.tellg();
-	}
+    char *GetS(char *buffer, int maxlen, void *pFP) {
+        if (PreMgr::sPreEnabled()) {
+            char *retval = PreMgr::pre_fgets(buffer, maxlen, (PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
+        return nullptr;
+    }
 
-	int Flush( void *pFP )
-	{
-		if (PreMgr::sPreEnabled())
-		{
-			int retval = PreMgr::pre_fflush((PreFile::FileHandle *) pFP);
-			if (PreMgr::sPreExecuteSuccess())
-				return retval;
-		}
-		return 0;
-	}
+    int PutS(const char *buffer, void *pFP) {
+        if (PreMgr::sPreEnabled()) {
+            int retval = PreMgr::pre_fputs(buffer, (PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
+        return 0;
+    }
+
+    int Eof(void *pFP) {
+        if (PreMgr::sPreEnabled()) {
+            int retval = PreMgr::pre_feof((PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
+        return 0;
+    }
+
+    int Seek(void *pFP, long offset, int origin) {
+        if (PreMgr::sPreEnabled()) {
+            int retval = PreMgr::pre_fseek((PreFile::FileHandle *) pFP, offset, origin);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
+
+        sFileHandle *h_file = (sFileHandle *) pFP;
+        h_file->file.seekg(offset, (origin == SEEK_CUR) ? std::ios::cur : ((origin == SEEK_SET) ? std::ios::beg
+                                                                                                : std::ios::end));
+        return h_file->file.tellg();
+    }
+
+    int Flush(void *pFP) {
+        if (PreMgr::sPreEnabled()) {
+            int retval = PreMgr::pre_fflush((PreFile::FileHandle *) pFP);
+            if (PreMgr::sPreExecuteSuccess())
+                return retval;
+        }
+        return 0;
+    }
 
 } // namespace File
 
